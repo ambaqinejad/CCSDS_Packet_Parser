fc4e2ba4f38d0dd4676737dd8a3b77c7513c5261
ambaqinejad <ambaqinejad@gmail.com>
2025-02-03 17:18:08 +0330

    wip

--- CCSDS/CCSDS_Packet.cpp
+++ CCSDS/CCSDS_Packet.cpp
@@ -1,50 +1,25 @@
 #include "CCSDS_Packet.h"
 #include <iostream>
-#include <iomanip>
 #include <cstring>
-//#include <winsock2.h>
-#include <cstdint>
-
-
-using namespace std;
-
-////
-//// Created by ambaqinejad on 2/3/2025.
-////
-
-unsigned short CCSDS_Packet::getSequence() const {
-    return sequence;
-}
-
-void CCSDS_Packet::setSequence(unsigned short _sequence) {
-    CCSDS_Packet::sequence = _sequence;
-}
-
-unsigned short CCSDS_Packet::getLength() const {
-    return length;
-}
-
-void CCSDS_Packet::setLength(unsigned short _length) {
-    CCSDS_Packet::length = _length;
-}
-
-const std::vector<uint8_t> &CCSDS_Packet::getPayload() const {
-    return payload;
-}
-
-void CCSDS_Packet::setPayload(const std::vector<uint8_t> &_payload) {
-    CCSDS_Packet::payload = _payload;
-}
-
-unsigned short CCSDS_Packet::getHeader() const {
-    return header;
-}
-
-void CCSDS_Packet::setHeader(unsigned short _header) {
-    CCSDS_Packet::header = _header;
-}
+#include <iomanip>
 
 CCSDS_Packet::CCSDS_Packet() : header(0), sequence(0), length(0) {}
+
+uint16_t CCSDS_Packet::getSequence() const { return sequence; }
+
+void CCSDS_Packet::setSequence(uint16_t _sequence) { sequence = _sequence; }
+
+uint16_t CCSDS_Packet::getLength() const { return length; }
+
+void CCSDS_Packet::setLength(uint16_t _length) { length = _length; }
+
+uint16_t CCSDS_Packet::getHeader() const { return header; }
+
+void CCSDS_Packet::setHeader(uint16_t _header) { header = _header; }
+
+const std::vector<uint8_t> &CCSDS_Packet::getPayload() const { return payload; }
+
+void CCSDS_Packet::setPayload(const std::vector<uint8_t> &_payload) { payload = _payload; }
 
 void CCSDS_Packet::parsePacket(const vector<uint8_t> &rawData) {
     if (rawData.size() < 6) {
@@ -55,22 +30,39 @@ void CCSDS_Packet::parsePacket(const vector<uint8_t> &rawData) {
     memcpy(&header, rawData.data(), 2);
     memcpy(&sequence, rawData.data() + 2, 2);
     memcpy(&length, rawData.data() + 4, 2);
-//    length = ntohs(length);
 
-    // extract payload
     payload.assign(rawData.begin() + 6, rawData.end());
 }
 
-// Display parsed packet data
 void CCSDS_Packet::printPacket() const {
     cout << "CCSDS Packet Header Info:" << endl;
     cout << "Version: " << ((header >> 13) & 0x07) << endl;
     cout << "Type: " << ((header >> 12) & 0x01) << endl;
-    cout << "APID " << (header & 0x07FF) << endl;
-    cout << "Packet Sequence Count: " << ((sequence & 0x3FFF)) << endl;
+    cout << "APID: " << (header & 0x07FF) << endl;
+    cout << "Packet Sequence Count: " << (sequence & 0x3FFF) << endl;
     cout << "Payload Data: ";
     for (uint8_t byte: payload) {
-        cout << hex << setw(2) << setfill('0') << (int)byte << " ";
+        cout << hex << setw(2) << setfill('0') << (int) byte << " ";
     }
     cout << dec << endl;
+}
+
+// C-Compatible Functions
+extern "C" {
+CCSDS_Packet *create_packet() {
+    return new CCSDS_Packet();
+}
+
+void destroy_packet(CCSDS_Packet *packet) {
+    delete packet;
+}
+
+void parse_packet(CCSDS_Packet *packet, uint8_t *data, int length) {
+    vector<uint8_t> rawData(data, data + length);
+    packet->parsePacket(rawData);
+}
+
+void print_packet(CCSDS_Packet *packet) {
+    packet->printPacket();
+}
 }
--- CCSDS/CCSDS_Packet.h
+++ CCSDS/CCSDS_Packet.h
@@ -1,7 +1,3 @@
-//
-// Created by ambaqinejad on 2/3/2025.
-//
-
 #ifndef CCSDS_PARSER_CCSDS_PACKET_H
 #define CCSDS_PARSER_CCSDS_PACKET_H
 
@@ -10,22 +6,16 @@
 #include <vector>
 
 using namespace std;
+
 class CCSDS_Packet {
 public:
-    uint16_t getSequence() const;
-
+    [[nodiscard]] uint16_t getSequence() const;
     void setSequence(uint16_t sequence);
-
-    uint16_t getLength() const;
-
+    [[nodiscard]] uint16_t getLength() const;
     void setLength(uint16_t length);
-
-    const vector<uint8_t> &getPayload() const;
-
+    [[nodiscard]] const vector<uint8_t> &getPayload() const;
     void setPayload(const vector<uint8_t> &payload);
-
-    uint16_t getHeader() const;
-
+    [[nodiscard]] uint16_t getHeader() const;
     void setHeader(uint16_t header);
 
     // Constructor
@@ -38,11 +28,18 @@ public:
     void printPacket() const;
 
 private:
-    uint16_t header; // First 2 bytes: Version (3), Type (1), SecHdrFlag (1), APID (11)
-    uint16_t sequence; // Next 2 bytes: Seq Flags (2), Packet Sequence Count (14)
-    uint16_t length; // Next 2 bytes: Packet Length (excluding header)
-    vector <uint8_t> payload; // Payload Data
+    uint16_t header;
+    uint16_t sequence;
+    uint16_t length;
+    vector<uint8_t> payload;
 };
 
+// C Wrapper for Python Compatibility
+extern "C" {
+CCSDS_Packet* create_packet();
+void destroy_packet(CCSDS_Packet* packet);
+void parse_packet(CCSDS_Packet* packet, uint8_t* data, int length);
+void print_packet(CCSDS_Packet* packet);
+}
 
-#endif //CCSDS_PARSER_CCSDS_PACKET_H
+#endif // CCSDS_PARSER_CCSDS_PACKET_H
